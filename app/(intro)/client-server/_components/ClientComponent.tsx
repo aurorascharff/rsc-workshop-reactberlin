01b9f8bebf6a71aa3cb17868c6409e905670cabc
Aurora Scharff <aurora.sofie@gmail.com>
2024-12-12 15:11:24 +0100

    Update

--- app/(intro)/client-server/_components/ClientComponent.tsx
+++ app/(intro)/client-server/_components/ClientComponent.tsx
@@ -2,7 +2,14 @@
 
 import React from 'react';
 
-export default function ClientComponent() {
+type Props = {
+  content: React.ReactNode;
+  children: React.ReactNode;
+};
+
+export default function ClientComponent({ content, children }: Props) {
+  console.log('ClientComponent');
+  // If we need browser stuff like onClick, useEffect, useState, alert, window.location, we need to create a client component
   return (
     <div className="rounded border border-red-500 p-4">
       <button
@@ -12,6 +19,8 @@ export default function ClientComponent() {
       >
         Click me
       </button>
+      {content}
+      {children}
     </div>
   );
 }
