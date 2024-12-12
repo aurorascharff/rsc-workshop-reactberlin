9593085f75acaf28c4ee2807173a63cae742a6eb
Aurora Scharff <aurora.sofie@gmail.com>
2024-12-12 16:17:41 +0100

    highlight contactbutton

--- components/ContactButton.tsx
+++ components/ContactButton.tsx
@@ -1,10 +1,16 @@
+'use client';
+
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import React from 'react';
 import { cn } from '@/utils/cn';
+import { useSafeSearchParams } from '@/validations/routeSchema';
 import type { Contact } from '@prisma/client';
 
 export default function ContactButton({ contact }: { contact: Contact }) {
-  const isActive = false;
+  const pathname = usePathname();
+  const isActive = pathname.includes(contact.id);
+  const { q } = useSafeSearchParams('home');
 
   return (
     <Link
@@ -12,7 +18,7 @@ export default function ContactButton({ contact }: { contact: Contact }) {
         isActive ? 'bg-primary text-white' : 'hover:bg-gray',
         'flex w-full items-center justify-between gap-4 overflow-hidden whitespace-pre rounded-lg p-2 hover:no-underline',
       )}
-      href={`/contacts/${contact.id}`}
+      href={`/contacts/${contact.id}?q=${q}`}
     >
       {contact.first || contact.last ? (
         <>
