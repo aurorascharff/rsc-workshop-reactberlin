d5e23ebc83bae4699414cc10aacc5732e77c0b39
Aurora Scharff <aurora.sofie@gmail.com>
2024-12-12 15:33:10 +0100

    Update

---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function NotFound() {
+  return (
+    <div>
+      <h1>404 not found</h1>
+      Could not find the page you were looking for
+    </div>
+  );
+}
