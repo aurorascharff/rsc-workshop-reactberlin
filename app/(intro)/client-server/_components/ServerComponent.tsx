a1d80e0b6904f8dd5b47d08a9bd72effbdfa14f4
Aurora Scharff <aurora.sofie@gmail.com>
2024-12-12 14:55:57 +0100

    Update

---
+++ app/(intro)/client-server/_components/ServerComponent.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { prisma } from '@/db';
+
+export default async function ServerComponent() {
+  const data = await prisma.contact.findMany();
+
+  return (
+    <div className="rounded border border-blue-500 p-4">
+      ServerComponent
+      {data[0].first}
+    </div>
+  );
+}
--- app/(intro)/client-server/page.tsx
+++ app/(intro)/client-server/page.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import ServerComponent from './_components/ServerComponent';
 
 export default function Page() {
-  return <div>Page</div>;
+  return (
+    <div>
+      <ServerComponent />
+    </div>
+  );
 }
